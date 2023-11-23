1a0f9837bd975cff3d2d2c65029c6649e2cbb964
PHANTOMGOD <achharyavedant@gmail.com>
2023-11-23 15:11:46 +0530

    Update route.ts

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -1,149 +1,171 @@
-import dotenv from "dotenv";
-import { StreamingTextResponse, LangChainStream } from "ai";
-import { auth, currentUser } from "@clerk/nextjs";
-import { Replicate } from "langchain/llms/replicate";
+import { MemoryManager } from "@/lib/memory";
+import db from "@/lib/prismadb";
+
+import { OpenAI } from "langchain/llms/openai";
 import { CallbackManager } from "langchain/callbacks";
+import { PromptTemplate } from "langchain/prompts";
+import { LLMChain } from "langchain/chains";
+
+
+import { rateLimit } from "@/lib/rate-limit";
+import { currentUser } from "@clerk/nextjs";
+import { LangChainStream, StreamingTextResponse } from "ai";
 import { NextResponse } from "next/server";
 
-import { MemoryManager } from "@/lib/memory";
-import { rateLimit } from "@/lib/rate-limit";
-import prismadb from "@/lib/prismadb";
+export async function POST(req: Request,
+    { params }: { params: { chatId: string } }
+) {    
+    try {
 
-dotenv.config({ path: `.env` });
+        const { prompt } = await req.json();
+        const user = await currentUser();
 
-export async function POST(
-  request: Request,
-  { params }: { params: { chatId: string } }
-) {
-  try {
-    const { prompt } = await request.json();
-    const user = await currentUser();
-
-    if (!user || !user.firstName || !user.id) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const identifier = request.url + "-" + user.id;
-    const { success } = await rateLimit(identifier);
-
-    if (!success) {
-      return new NextResponse("Rate limit exceeded", { status: 429 });
-    }
-
-    const companion = await prismadb.companion.update({
-      where: {
-        id: params.chatId
-      },
-      data: {
-        messages: {
-          create: {
-            content: prompt,
-            role: "user",
-            userId: user.id,
-          },
-        },
-      }
-    });
-
-    if (!companion) {
-      return new NextResponse("Companion not found", { status: 404 });
-    }
-
-    const name = companion.id;
-    const companion_file_name = name + ".txt";
-
-    const companionKey = {
-      companionName: name!,
-      userId: user.id,
-      modelName: "mistral-7b",
-    };
-    const memoryManager = await MemoryManager.getInstance();
-
-    const records = await memoryManager.readLatestHistory(companionKey);
-    if (records.length === 0) {
-      await memoryManager.seedChatHistory(companion.seed, "\n\n", companionKey);
-    }
-    await memoryManager.writeToHistory("User: " + prompt + "\n", companionKey);
-
-    // Query Pinecone
-
-    const recentChatHistory = await memoryManager.readLatestHistory(companionKey);
-
-    // Right now the preamble is included in the similarity search, but that
-    // shouldn't be an issue
-
-    const similarDocs = await memoryManager.vectorSearch(
-      recentChatHistory,
-      companion_file_name
-    );
-
-    let relevantHistory = "";
-    if (!!similarDocs && similarDocs.length !== 0) {
-      relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
-    }
-    const { handlers } = LangChainStream();
-    // Call Replicate for inference
-    const model = new Replicate({
-      model:
-        "a16z-infra/mistral-7b-v0.1:3e8a0fb6d7812ce30701ba597e5080689bef8a013e5c6a724fafb108cc2426a0",
-      input: {
-        max_length: 2048,
-      },
-      apiKey: process.env.REPLICATE_API_TOKEN,
-      callbackManager: CallbackManager.fromHandlers(handlers),
-    });
-
-    // Turn verbose on for debugging
-    model.verbose = true;
-
-    const resp = String(
-      await model
-        .call(
-          `
-        ONLY generate plain sentences without prefix of who is speaking. DO NOT use ${companion.name}: prefix. 
-
-        ${companion.instructions}
-
-        Below are relevant details about ${companion.name}'s past and the conversation you are in.
-        ${relevantHistory}
-
-
-        ${recentChatHistory}\n${companion.name}:`
-        )
-        .catch(console.error)
-    );
-
-    const cleaned = resp.replaceAll(",", "");
-    const chunks = cleaned.split("\n");
-    const response = chunks[0];
-
-    await memoryManager.writeToHistory("" + response.trim(), companionKey);
-    var Readable = require("stream").Readable;
-
-    let s = new Readable();
-    s.push(response);
-    s.push(null);
-    if (response !== undefined && response.length > 1) {
-      memoryManager.writeToHistory("" + response.trim(), companionKey);
-
-      await prismadb.companion.update({
-        where: {
-          id: params.chatId
-        },
-        data: {
-          messages: {
-            create: {
-              content: response.trim(),
-              role: "system",
-              userId: user.id,
-            },
-          },
+        if (!user) {
+            return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
         }
-      });
-    }
 
-    return new StreamingTextResponse(s);
-  } catch (error) {
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-};
+        const identifier = `${user.id}-${params.chatId}`;
+        const { success } = await rateLimit(identifier);
+
+        if (!success) {
+            return NextResponse.json({ message: 'Rate limit exceeded' }, { status: 429 });
+        }
+
+        // update the companion with the new prompt and return the updated companion
+        const companion = await db.companion.update({
+            where: {
+                id: params.chatId
+            },
+
+            data: {
+                messages: {
+                    create: {
+                        content: prompt,
+                        role: 'user',
+                        userId: user.id,
+                    }
+                }
+            }
+        });
+
+        if (!companion) {
+            return NextResponse.json({ message: 'Companion not found' }, { status: 404 });
+        }
+
+        const companion_id = companion.id;
+        const pine_cone_identifier = companion_id + '.pine_cone';
+
+        // let's create companion key for redis
+        const companionKey = {
+            companionName: companion_id,
+            modelName: 'llama2-13b',
+            userId: user.id
+        };
+
+        const memManager = await MemoryManager.getInstance();
+
+        // if it's the first time talking to the companion, we need to seed the chat history
+        const records = await memManager.readLatestHistory(companionKey);
+        if (!records || records.length == 0) {
+            await memManager.seedChatHistory(companion.seed, '\n\n', companionKey);
+        }
+
+        // write the new prompt to the chat history in the format of our seed!!
+        await memManager.writeToHistory("User: " + prompt + "\n", companionKey);
+
+        // let's get similar docs from our pinecone db
+        const recentChatHistory = await memManager.readLatestHistory(companionKey);
+
+        const similarDocs = await memManager.vectorSearch(
+            recentChatHistory,
+            pine_cone_identifier
+        )
+
+        let relevantHistory = "";
+        if (!!similarDocs && similarDocs.length !== 0) {
+            relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
+        }
+
+        console.log("RELEVANT HISTORY: ", relevantHistory);
+
+        const { handlers } = LangChainStream();
+
+        const model = new OpenAI(
+            {
+                modelName: "gpt-3.5-turbo-16k",
+                openAIApiKey: process.env.OPENAI_API_KEY,
+                callbackManager: CallbackManager.fromHandlers(handlers),
+            }
+        )
+
+        model.verbose = true;
+        const chainPrompt = PromptTemplate.fromTemplate(
+            `
+            ONLY generate plain sentences without prefix of who is speaking. DO NOT use ${companion.name}: prefix. 
+            
+            You are ${companion.name} and are currently talking to ${currentUser.name}.
+            
+            ${companion.instructions}
+            
+            Below are relevant details about ${companion.name}'s past and the conversation you are in.
+            ${relevantHistory}
+            
+            Below is a relevant conversation history
+            ${recentChatHistory}\n${companion.name}
+            `
+        )
+
+        const chain = new LLMChain({
+            llm: model,
+            prompt: chainPrompt
+        })
+
+        const response = await chain
+            .call({
+                relevantHistory,
+                recentChatHistory: recentChatHistory,
+            })
+            .catch(
+                console.error
+            )
+
+        const real_response = response!.text;
+        
+        if (real_response !== undefined && real_response.length > 1) {
+            memManager.writeToHistory("" + real_response.trim(), companionKey);
+
+            // upsert it into the pinecone db
+            const current_history = `${real_response.trim()}`;
+            await memManager.UpsertChatHistory(
+                recentChatHistory + "\n" + current_history,
+                pine_cone_identifier
+            );
+
+            await db.companion.update({
+                where: {
+                    id: params.chatId
+                },
+                data: {
+                    messages: {
+                        create: {
+                            content: real_response,
+                            role: "system",
+                            userId: user.id,
+                        },
+                    },
+                }
+            });
+        }
+
+        var Readable = require("stream").Readable;
+        let s = new Readable();
+        s.push(real_response);
+        s.push(null);
+
+        return new StreamingTextResponse(s);
+    }
+    catch (error) {
+        console.log('ERROR GETTING RESPONSE FROM CHATGPT: ', error);
+        return new NextResponse("Internal server error", { status: 500 });
+    }
+    }
